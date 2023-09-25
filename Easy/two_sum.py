534ae3f5c7f1c09a04d84ee1ec23d1b5c1a4adce
Gabriel Ozélo Braga <g247345@dac.unicamp.br>
2023-09-25 18:09:09 -0300

    Initial commit

---
+++ Easy/two_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index_1 = 0
+        index_2 = 1
+        compensation = 0
+        for x in nums:
+            for y in nums[1::]:
+                if x+y == target:
+                    if index_1 == index_2:
+                        index_2 += 1
+                    if x+nums[index_2] == target:
+                        return ([index_1, index_2])
+                index_2 += 1
+            index_1 += 1
+            index_2 = 1
